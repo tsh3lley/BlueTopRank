5959a7912ebbae7d7ec4437bba1649b2f1ab0c16
tsh3lley <thomasjshelley@gmail.com>
2015-09-04 00:50:24 -0400

    added statements to check RGB ranges

--- PixelCounter.java
+++ PixelCounter.java
@@ -12,19 +12,52 @@ import javax.swing.JFrame;
 		
 		public PixelCounter() { 
 			try { 
-				File input = new File("opencv.jpg"); 
+				File input = new File("bluerange.png"); 
 				image = ImageIO.read(input); 
 				width = image.getWidth();
 				height = image.getHeight();
+				
 				int count = 0;
+				int totalRed = 0;
+				int totalGreen = 0;
+				int totalBlue = 0;
+				int lowTotal = 255*3;
+				int lowRed = 255;
+				int lowGreen = 255;
+				int lowBlue = 255;
+				int highTotal = 0;
+				int highRed = 0;
+				int highGreen = 0;
+				int highBlue = 0;
 				
 				for(int i=0; i<height; i++){ 
 					for(int j=0; j<width; j++){ 
 						count++; 
-						Color c = new Color(image.getRGB(j, i)); 
-						System.out.println("S.No: " + count + " Red: " + c.getRed() +" Green: " + c.getGreen() + " Blue: " + c.getBlue());
+						Color c = new Color(image.getRGB(j, i));
+						lowTotal = (c.getRed() + c.getGreen() + c.getBlue() < lowTotal) ? (c.getRed() + c.getGreen() + c.getBlue()) : lowTotal;
+						lowRed = (c.getRed() < lowRed) ? c.getRed() : lowRed;
+						lowGreen = (c.getGreen() < lowGreen) ? c.getGreen() : lowGreen;
+						lowBlue = (c.getBlue() < lowBlue) ? c.getBlue() : lowBlue;
+
+						highTotal = (c.getRed() + c.getGreen() + c.getBlue() > highTotal) ? (c.getRed() + c.getGreen() + c.getBlue()) : highTotal;
+						highRed = (c.getRed() > highRed) ? c.getRed() : highRed;
+						highGreen = (c.getGreen() > highGreen) ? c.getGreen() : highGreen;
+						highBlue = (c.getBlue() > highBlue) ? c.getBlue() : highBlue;
+						
+						totalRed += c.getRed();
+						totalGreen += c.getGreen();
+						totalBlue += c.getBlue();
+						
+						
+						
+						//System.out.println("S.No: " + count + " Red: " + c.getRed() +" Green: " + c.getGreen() + " Blue: " + c.getBlue());
 					}
 				}
+				System.out.println("Total Range: " + lowTotal + " - " + highTotal);
+				System.out.println("Average Red: " + totalRed/count + " Range: " + lowRed + " - " + highRed);
+				System.out.println("Average Green: " + totalGreen/count + " Range: " + lowGreen + " - " + highGreen);
+				System.out.println("Average Blue: " + totalBlue/count + " Range: " + lowBlue + " - " + highBlue);
+				
 			}
 			
 			catch (Exception e){
